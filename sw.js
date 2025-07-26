79b5d602ed034ba9a33810838903e07fcc48f049
Silenced <sillencedplayz@gmail.com>
2025-07-26 20:54:46 +0800

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = "offline-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./jszip/jszip.js",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
